48cb8b3d9f9287cb6accdf71ccc9f9a3d23a4f5f
MakeYourOwnPizza <34776179+MakeYourOwnPizza@users.noreply.github.com>
2018-01-29 14:35:52 -0600

    ML week 1

---
+++ my functions/costFunctionJ.m
@@ -0,0 +1,14 @@
+function J = costFunctionJ(X, y, 
+theta)
+% X is the "design matrix" containing our training examples.
+% y is the calss labels
+% theta is hypothetical parameters
+
+% number of training examples
+m = size(X,1);	
+% predictions of hypothesis on all m examples
+predictions = X*theta;
+%squared errors
+sqrErrors = (predictions - y).^2;
+
+J = 1/(2*m) * sum(sqrErrors);
